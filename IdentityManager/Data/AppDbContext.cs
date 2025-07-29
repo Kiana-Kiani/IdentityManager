4ae06462a47c4018d166b8e121e4581194ee075a
Kiana <87224460+Kiana-Kiani@users.noreply.github.com>
2025-07-29 14:44:33 +0200

    Add project files.

---
+++ IdentityManager/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityManager.Data
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+
+    }
+}
